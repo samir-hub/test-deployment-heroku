40c86cc117331bc78c32a6ee1f8954f0a9ebc22a
Samir Lilienfeld <slilienfeld@ccah.com>
2020-08-20 16:31:04 -0400

    created app

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+  });
+
+app.get('/image', (req, res) => {
+  res.sendFile('C:/Users/slilienfeld/Tutorials/test-deployment-heroku/assets/image1.jpg')
+});
+
+app.get('/video', (req, res) => {
+    res.sendFile('C:/Users/slilienfeld/Tutorials/test-deployment-heroku/assets/video1.mp4')
+  });
+  
+
+app.listen(8000, () => {
+  console.log('Example app listening on port 8000!')
+});
