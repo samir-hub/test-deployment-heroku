78819ba76fa62ccc20be97ef927cff5113badb29
Samir Lilienfeld <slilienfeld@ccah.com>
2020-08-20 16:45:04 -0400

    first commit

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ app.get('/', (req, res) => {
     res.send('Hello World')
   });
 
-// app.get('/image', (req, res) => {
-//   res.sendFile('C:/Users/slilienfeld/Tutorials/test-deployment-heroku/assets/image1.jpg')
-// });
+app.get('/image', (req, res) => {
+  res.sendFile('C:/Users/slilienfeld/Tutorials/test-deployment-heroku/assets/image1.jpg')
+});
 
-// app.get('/video', (req, res) => {
-//     res.sendFile('C:/Users/slilienfeld/Tutorials/test-deployment-heroku/assets/video1.mp4')
-//   });
+app.get('/video', (req, res) => {
+    res.sendFile('C:/Users/slilienfeld/Tutorials/test-deployment-heroku/assets/video1.mp4')
+  });
   
 
 app.listen(8000, () => {
